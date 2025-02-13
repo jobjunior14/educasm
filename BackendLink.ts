db8a679315ca796aa9a4b47e5ba1700e77769afb
Job Junior <102473673+jobjunior14@users.noreply.github.com>
2025-02-13 07:43:46 +0200

    Update Backend_Link to use production API URL

--- BackendLink.ts
+++ BackendLink.ts
@@ -1,4 +1,5 @@
 export enum Backend_Link {
-  "LOCAL" = "http://localhost:5000/api/v1/ai",
+  // "LOCAL" = "http://localhost:5000/api/v1/ai",
+  "LOCAL" = "https://server-educasm.onrender.com/api/v1/ai",
   // "PRODUCTION" = "https://educasm.herokuapp.com",
 }
